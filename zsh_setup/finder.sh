86154123b7e9ef9805963714ab495de458e1a61a
BMilliet <brunomilliet89@gmail.com>
2023-06-25 11:55:25 -0300

    improve search

--- zsh_setup/finder.sh
+++ zsh_setup/finder.sh
@@ -3,16 +3,24 @@
 
 psf() {
   result=$(find "$HOME_CONFIG" "$WORKSPACE" \( -name .git -o -name .build \) -prune -o -print | fzf)
-
-  if [ -n "$result" ]; then
-    cd $result
-  fi
+  handle_search $result
 }
 
 sf() {
   result=$(find . \( -name .git -o -name .build \) -prune -o -print | fzf)
+  handle_search $result
+}
+
+handle_search() {
+  local result="$1"
 
   if [ -n "$result" ]; then
-    cd $result
+    if [ -d "$result" ]; then
+      cd $result
+    elif [ -f "$result" ]; then
+      $EDITOR $result
+    else
+      echo "${RED}not${NO_COLOR} dir or file"
+    fi
   fi
 }
