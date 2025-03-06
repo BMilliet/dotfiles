e9bb8b4a06ac5b8f9efeb737221ff1c23b09adee
BMilliet <brunomilliet89@gmail.com>
2025-03-06 12:15:28 +0000

    chore(main): added LANG and spm init

--- zsh_my_stuff.sh
+++ zsh_my_stuff.sh
@@ -1,5 +1,7 @@
 #### ENVS
 
+export LANG=en_US.UTF-8
+
 EDITOR=nvim
 WORKSPACE=~/workspace
 
@@ -16,6 +18,8 @@ alias vi="nvim"
 alias vim="nvim"
 
 alias add_custom="nvim ~/.config/dotfiles/zsh_custom.sh"
+
+alias swift_init="swift package init --type executable"
 
 #### FZF
 
@@ -50,16 +54,17 @@ zhelp() {
   echo -e "\033[1;34m#### Help: Commands and Aliases ####\033[0m"
   echo ""
   echo -e "\033[1;35m 🚀 Available Aliases:\033[0m"
-  echo -e "\033[1;36m  docs        \033[0m- Open personal documents with $EDITOR"
-  echo -e "\033[1;36m  work        \033[0m- Go to the workspace directory ($WORKSPACE)"
-  echo -e "\033[1;36m  zso         \033[0m- Reload the ~/.zshrc file"
-  echo -e "\033[1;36m  nconfig     \033[0m- Open Neovim configuration"
-  echo -e "\033[1;36m  dotconfig   \033[0m- Open dotfiles configuration"
-  echo -e "\033[1;36m  cat         \033[0m- Replace 'cat' with the 'bat' command"
-  echo -e "\033[1;36m  vi          \033[0m- Replace 'vi' with $EDITOR"
-  echo -e "\033[1;36m  vim         \033[0m- Replace 'vim' with $EDITOR"
-  echo -e "\033[1;36m  add_custom  \033[0m- Edit zsh customizations file"
-
+  echo -e "\033[1;36m  docs                              \033[0m- Open personal documents with $EDITOR"
+  echo -e "\033[1;36m  work                              \033[0m- Go to the workspace directory ($WORKSPACE)"
+  echo -e "\033[1;36m  zso                               \033[0m- Reload the ~/.zshrc file"
+  echo -e "\033[1;36m  nconfig                           \033[0m- Open Neovim configuration"
+  echo -e "\033[1;36m  dotconfig                         \033[0m- Open dotfiles configuration"
+  echo -e "\033[1;36m  cat                               \033[0m- Replace 'cat' with the 'bat' command"
+  echo -e "\033[1;36m  vi                                \033[0m- Replace 'vi' with $EDITOR"
+  echo -e "\033[1;36m  vim                               \033[0m- Replace 'vim' with $EDITOR"
+  echo -e "\033[1;36m  add_custom                        \033[0m- Edit zsh customizations file"
+  echo -e "\033[1;36m  swift new package (executable)    \033[0m- Init new swift executable package"
+  echo ""
   echo -e "\n\033[1;35m 🧰 Available Functions:\033[0m"
   echo -e "\033[1;36m  psf         \033[0m- Search for files in the workspace ($WORKSPACE) using fzf"
   echo -e "\033[1;36m  sf          \033[0m- Search for files in the current directory using fzf"
